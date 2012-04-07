37ec34dbf1b0527912ea20b334aae3e1243730dd
Cody Schafer <cpschafer@gmail.com>
2012-04-07 14:53:06 -0500

    printer

--- dab/input.hh
+++ dab/input.hh
@@ -31,10 +31,20 @@ public:
 	void read_input(void)
 	{
 		std::cin >> player_num;
+		bool input_funky = false;
 
 		for (int r = 0; r < bw; r++) {
 			for (int c = 0; c < bw; c++) {
 				std::cin >> raw[r][c];
+				if (std::cin.fail()) {
+					std::cin.clear();
+					if (!input_funky) {
+						std::cerr << "Input file invalid (probably missing last row). Assuming '0'."
+							  << "Your only warning" << std::endl;
+						input_funky = true;
+					}
+					raw[r][c] = 0;
+				}
 			}
 		}
 	}
@@ -45,7 +55,29 @@ public:
 
 		for (int r = 0; r < bw; r++) {
 			for (int c = 0; c < bw; c++) {
-				std::cout << raw[r][c] << ' ';
+				bool col_odd = c%2;
+				bool row_odd = r%2;
+
+				if (!col_odd && !row_odd) {
+					std::cout << '.';
+				} else if (col_odd && !row_odd) {
+					if (raw[r][c] == 1)
+						std::cout << '-';
+					else
+						std::cout << ' ';
+				} else if (col_odd && row_odd) {
+					if (raw[r][c] != 0)
+						std::cout << raw[r][c];
+					else
+						std::cout << ' ';
+				} else if (!col_odd && row_odd) {
+					if (raw[r][c] == 1)
+						std::cout << '|';
+					else
+						std::cout <<' ';
+				} else {
+					std::cout << '?';
+				}
 			}
 			std::cout << std::endl;
 		}
